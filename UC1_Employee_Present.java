5b13c5ac09ac750ae54e3f8c9a76af86c6cd1aba
Aman <raiaman21@gmail.com>
2022-06-29 14:26:58 +0530

    UC1 solved

---
+++ UC1_Employee_Present.java
@@ -0,0 +1,16 @@
+public class UC1_Employee_Present {
+public static void main(String[] args) {
+	
+	int Employee_present=1;
+	double employeeCheck=(int) (Math.random() * 10);
+	
+	if (employeeCheck==Employee_present){
+		System.out.println("Employee is 'PRESENT'");
+	}
+	else {
+		System.out.println("Employee is 'ABSENT'");
+	}
+
+}
+
+}
