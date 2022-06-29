f4b56a74e6963fa784fb6cee87d36ed0b7ecdb4e
Aman <raiaman21@gmail.com>
2022-06-29 14:55:59 +0530

    UC4 Solved

---
+++ employeewage.java
@@ -0,0 +1,27 @@
+public class employeewage {
+    public static void main(String[] args) {
+        
+        final int PartTime=1;
+        final int FullTime=2;
+            int employeeCheck=(int) (Math.random() * 10)%3;
+        final int wagePerHrs=20;
+            int workingHrs=0;
+            
+                
+            switch(employeeCheck) 
+            {
+            case FullTime:
+                System.out.println("Employee is present as 'FULLTIME' ");
+                workingHrs=8;
+                break;
+            case PartTime:
+                System.out.println("Employee is present as 'PARTTIME' ");
+                workingHrs=4;
+                break;
+            default:
+                System.out.println("Employee is absent");
+            }
+            int employeeWage = workingHrs*wagePerHrs;
+            System.out.println("Employee Daily wages is as per working hour " + employeeWage);
+        }
+        }
